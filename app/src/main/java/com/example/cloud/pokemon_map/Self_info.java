fbccab2407fda476e3a324a4a7aba7567706c534
Junjie Zheng <zzj@outlook.com>
2018-01-04 21:12:22 +0800

    滑动菜单的用户名修改功能完成.

--- app/src/main/java/com/example/cloud/pokemon_map/Self_info.java
+++ app/src/main/java/com/example/cloud/pokemon_map/Self_info.java
@@ -3,6 +3,8 @@ package com.example.cloud.pokemon_map;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.cloud.pokemon_map.db.User;
@@ -43,16 +46,23 @@ public class Self_info extends AppCompatActivity {
         TextView text_name = (TextView) findViewById(R.id.self_info_textview_username);
         TextView text_id = (TextView) findViewById(R.id.self_info_textview_userid);
 
+        String name = "";
+        String id = "";
+
+        // 设置用户信息
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
-
         List<User> users = DataSupport.where("user_name = ?", userName).find(User.class);
         for (User user: users) {
             Log.d("Self_info", "user id is " + user.getId());
 
-            text_name.setText(user.getUser_name());
-            text_id.setText(String.valueOf(user.getId()));
+            name = user.getUser_name();
+            id = String.valueOf(user.getId());
 
+            text_name.setText(name);
+            text_id.setText(id);
+
+            // 设置头像
             if (!Objects.equals(user.getUser_headshot(), "default")) {
                 Log.d("Self_info", "headshot not default");
             }
@@ -65,6 +75,24 @@ public class Self_info extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        // 对滑动菜单中的内容进行更改
+        NavigationView navView = (NavigationView) findViewById(R.id.self_info_nav_view);
+        View header = navView.getHeaderView(0);
+        TextView menu_name = (TextView) header.findViewById(R.id.drawer_menu_textview_username);
+        TextView menu_id = (TextView) header.findViewById(R.id.drawer_menu_textview_userid);
+        menu_name.setText(name);
+        menu_id.setText(id);
+
+        // 滑动菜单项点击事件
+        navView.setCheckedItem(R.id.drawer_menu_profile);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
